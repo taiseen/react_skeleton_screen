c04e89efd857025335e2dc5f3bd11609c2a85358
taiseen <taiseen.cse@gmail.com>
2021-06-05 08:51:42 +0600

    all data fetch ==> OK

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Basic React Skeleton Screen App | 
+# Basic React Skeleton Screen App | [Live Link](hhhh)
 
 
 ## Topic of Focus:
 - setTimeout | async await fetch data 
 
 
-<img src="./src/img/demo.png">
+<!-- <img src="./src/img/demo.png"> -->
--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,8 +1,35 @@
+import { useEffect, useState } from "react";
 
 const Articles = () => {
+
+    const [articles, setArticle] = useState(null);
+
+    useEffect(() => {
+
+        setTimeout(async () => {
+
+            const url = `https://jsonplaceholder.typicode.com/posts`;
+            const res = await fetch(url);
+            const data = await res.json();
+            setArticle(data);
+
+        }, 5000);
+    }, []);
+
     return (
         <div className="articles">
             <h2>Articles</h2>
+
+            {articles && (
+                articles.map(article => {
+                    const { id, title, body } = article;
+                    return <div className="article" key={id}>
+                        <h3>{title}</h3>
+                        <p>{body}</p>
+                    </div>
+                })
+            )}
+            {!articles && (<h1>Loading...</h1>)}
         </div>
     );
 };
